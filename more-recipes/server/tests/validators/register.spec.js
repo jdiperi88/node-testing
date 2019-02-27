0c64f1b71eb753181f9dcd713778544c4202201f
jdiperi88 <jdiperi88@yahoo.com>
2019-02-27 16:02:03 -0500

    fixed all the bugs in the more_recipes test app and added more unit tests to test out the registerValidator

---
+++ more-recipes/server/tests/validators/register.spec.js
@@ -0,0 +1,95 @@
+import validators from "../../validators";
+import { User } from "../../database/models";
+const { RegisterUserValidator } = validators;
+
+describe("The RegisterUserValidator Class", () => {
+  describe("The validateName Function", () => {
+    test("The validateName function adds a required error to the errors array if name is not provided.", () => {
+      // ARRANGE ACTION ASSERTION 3 STEP CYCLE
+      //ARRANGE
+      const validator = new RegisterUserValidator({
+        email: "jdiperi88@gmail.com"
+      });
+      //ACTION
+      validator.validateName();
+      const errors = validator.errors;
+      //ASSERT
+      expect(errors).toEqual(["The name is required."]);
+    });
+
+    test("adds an error if name is less than 5 characters", () => {
+      //ARRANGE
+      const validator = new RegisterUserValidator({
+        name: "joey"
+      });
+      //ACTION
+      validator.validateName();
+      //assert
+      expect(validator.errors).toEqual([
+        "The name must be longer than 5 characters."
+      ]);
+    });
+  });
+
+  describe("The validatePassword Function", () => {
+    test("The validatePassword function adds a required error to the errors array if Password is not provided.", () => {
+      // ARRANGE ACTION ASSERTION 3 STEP CYCLE
+      //ARRANGE
+      const validator = new RegisterUserValidator({
+        email: "jdiperi88@gmail.com"
+      });
+      //ACTION
+      validator.validatePassword();
+      const errors = validator.errors;
+      //ASSERT
+      expect(errors).toEqual(["The password is required."]);
+    });
+
+    test("adds an error if password is less than 5 characters", () => {
+      //ARRANGE
+      const validator = new RegisterUserValidator({
+        password: "joey"
+      });
+      //ACTION
+      validator.validatePassword();
+      //assert
+      expect(validator.errors).toEqual([
+        "The password must be longer than 5 characters."
+      ]);
+    });
+  });
+
+  describe("The validate email function test", () => {
+    test("adds a required error to the errors array if email is not provided.", async () => {
+      // ARRANGE ACTION ASSERTION 3 STEP CYCLE
+      //ARRANGE
+      const validator = new RegisterUserValidator({
+        name: "jdiperi88@gmail.com"
+      });
+      //ACTION
+      await validator.validateEmail();
+      const errors = validator.errors;
+      //ASSERT
+      expect(errors).toEqual(["The email is required."]);
+    });
+
+    test("adds an email taken error if user already exists with that email", async () => {
+      // ARRANGE ACTION ASSERTION 3 STEP CYCLE
+      await User.destroy({ where: {} });
+      //ARRANGE
+      await User.create({
+        name: "Joey",
+        email: "example@example.com",
+        password: "password"
+      });
+      const validator = new RegisterUserValidator({
+        email: "example@example.com"
+      });
+      //ACTION
+      await validator.validateEmail();
+      const errors = validator.errors;
+      //ASSERT
+      expect(errors).toEqual(["A user with this email already exists."]);
+    });
+  });
+});
